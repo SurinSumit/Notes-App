f1c9ffd4d2f9e2961fa5fce003f3c020d6288f9d
Sumit Surin <129991095+SurinSumit@users.noreply.github.com>
2024-10-16 14:28:20 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     useEffect(() => {
         // Fetch notes from the server
         axios
-            .get('https://notes-m6zf99809-sumit-surins-projects.vercel.app/api/notes')
+            .get('https://notes-app-ochre-delta.vercel.app/api/notes')
             .then((response) => setNotes(response.data))
             .catch((error) => console.error('Error fetching notes:', error));
     }, []);
@@ -22,7 +22,7 @@ const App = () => {
     const handleAddNote = () => {
         // Add a new note to the server
         axios
-            .post('https://notes-m6zf99809-sumit-surins-projects.vercel.app/api/notes', { title, content })
+            .post('https://notes-app-ochre-delta.vercel.app/api/notes', { title, content })
             .then((response) => {
                 setNotes([...notes, response.data]);
                 setTitle('');
@@ -33,7 +33,7 @@ const App = () => {
     const handleEditNote = (id, updatedTitle, updatedContent) => {
         // Update note by ID
         axios
-            .put(`https://notes-m6zf99809-sumit-surins-projects.vercel.app/api/notes/${id}`, {
+            .put(`https://notes-app-ochre-delta.vercel.app/api/notes/${id}`, {
                 title: updatedTitle,
                 content: updatedContent,
             })
@@ -49,7 +49,7 @@ const App = () => {
     const handleDeleteNote = (id) => {
         // Delete note by ID
         axios
-            .delete(`https://notes-m6zf99809-sumit-surins-projects.vercel.app/api/notes/${id}`)
+            .delete(`https://notes-app-ochre-delta.vercel.app/api/notes/${id}`)
             .then((response) => {
                 const updatedNotes = notes.filter((note) => note._id !== id);
                 setNotes(updatedNotes);
