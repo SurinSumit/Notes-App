255e331c506608bd1fe47e54370c66fa7407ca44
Sumit Surin <129991095+SurinSumit@users.noreply.github.com>
2024-10-16 14:22:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors(
     {
-        origin: ["https://notes-app-frontend-henna-delta.vercel.app/"],
+        origin: ["https://notes-app-frontend-henna-delta.vercel.app"],
         methods: ["POST","GET"],
         credentials: true
     }
